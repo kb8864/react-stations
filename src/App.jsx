351c70ea0a0bb7675951f55962590b87d5dd6219
kb8864 <yt7s858864@gmail.com>
2025-09-17 20:18:17 +0900

    APIを叩いて、ランダムな画像を表示

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,18 @@ export const App = () => {
   const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg");
 
 
+  //ランダムに画像を表示させるために追加
+  const handleNext = async () =>{
+    try {
+    const res = await fetch('https://dog.ceo/api/breeds/image/random');
+    if (!res.ok) throw new Error('Network error');
+    const data = await res.json();       
+    setDogUrl(data.message);     
+    } catch (e) {
+      console.log("画像取得失敗");
+    }
+  }
+
   return (
     <>
       <header className="app-header">
@@ -25,7 +37,8 @@ export const App = () => {
       <img src={dogUrl} alt="表示する犬の写真" />
 
       {/* ボタン追加 */}
-      <button onClick={() => setDogUrl('https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg')}>更新</button>
+      {/* <button onClick={() => setDogUrl('https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg')}>更新</button> */}
+      <button onClick={handleNext}>ランダムに次の犬の画像を表示</button>
     </main>
     </>
 

41f7c9b2012e86da0fb841487c667b0cddc5e2dc
kb8864 <yt7s858864@gmail.com>
2025-09-18 00:54:58 +0900

    コンポーネント分割とProps

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 // DO NOT DELETE
 
 import './App.css'
-import { useState } from "react";
+import {Header} from './Header.jsx';
+import {Description} from './Description.jsx';
 
 /**
  * @type {() => JSX.Element}
@@ -10,39 +11,18 @@ export const App = () => {
   const appName = "Dogアプリ";
   const description = " 犬の画像を表示するサイトです";
   // const dogImageUrl = "https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg";
-  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg");
-
-
-  //ランダムに画像を表示させるために追加
-  const handleNext = async () =>{
-    try {
-    const res = await fetch('https://dog.ceo/api/breeds/image/random');
-    if (!res.ok) throw new Error('Network error');
-    const data = await res.json();       
-    setDogUrl(data.message);     
-    } catch (e) {
-      console.log("画像取得失敗");
-    }
-  }
-
-  return (
+  return(
     <>
-      <header className="app-header">
-        <div className="container">
-          <h1 className="brand">{appName}</h1>
-        </div>
-    </header>
-    <main className = "container">
-      <p>{description}</p>
-      <img src={dogUrl} alt="表示する犬の写真" />
-
-      {/* ボタン追加 */}
-      {/* <button onClick={() => setDogUrl('https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg')}>更新</button> */}
-      <button onClick={handleNext}>ランダムに次の犬の画像を表示</button>
-    </main>
+    <Header title = {appName}></Header>
+    <Description description={description}></Description>
     </>
+  )  
+
+
+  
+    
 
     
-  )
+  
 }
 
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,9 @@
-// @ts-check
-
-export const Header = () => {
-  return <></>
+export const Header = ({ title = "Dogアプリ" }) => {
+  return (
+    <header className="app-header">
+      <div className="container">
+        <h1 className="brand">{title}</h1>
+      </div>
+    </header>
+  );
 }
-
-export default Header
